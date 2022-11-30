9fa8f5be8294540f5dfa34b6bc39b5b75d00a523
UrielCh <uriel.chemouni@gmail.com>
2022-11-30 08:08:51 +0200

    split code logic

--- islands/DarkMode.tsx
+++ islands/DarkMode.tsx
@@ -1,9 +1,13 @@
 import { useState } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
-import IconSun from "https://deno.land/x/tabler_icons_tsx@0.0.1/tsx/sun.tsx";
-import IconSunglasses from "https://deno.land/x/tabler_icons_tsx@0.0.1/tsx/sunglasses.tsx";
-import IconMoon from "https://deno.land/x/tabler_icons_tsx@0.0.1/tsx/moon.tsx";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+// import {
+//   MdBrightnessAuto,
+//   MdDarkMode,
+//   MdOutlineWbSunny,
+// } from "https://deno.land/x/react_icons@0.1.3/md/mod.ts";
+import { BsMoon, BsSun } from "https://deno.land/x/react_icons@0.1.3/bs/mod.ts";
+import { GrSystem } from "https://deno.land/x/react_icons@0.1.3/gr/mod.ts";
 
 interface CounterProps {
   start: number;
@@ -13,9 +17,9 @@ export default function DarkMode(props: CounterProps) {
   /**
    * Used to format mode as text in screen
    */
-  function getMode(): "light" | "dark" | "auto" {
+  function getMode(): "light" | "dark" | "system" {
     if (!IS_BROWSER) {
-      return "auto";
+      return "system";
     }
     if (localStorage.theme === "dark") {
       return "dark";
@@ -23,7 +27,7 @@ export default function DarkMode(props: CounterProps) {
     if (localStorage.theme) {
       return "light";
     }
-    return "auto";
+    return "system";
   }
 
   function updateMode() {
@@ -36,38 +40,38 @@ export default function DarkMode(props: CounterProps) {
 
   const [mode, setMode] = useState(getMode());
 
+  const setDarkModeOn = () => {
+    localStorage.theme = "dark";
+    updateMode();
+    setMode("dark");
+  };
+
+  const setDarkModeAuto = () => {
+    delete localStorage.theme;
+    updateMode();
+    setMode("system");
+  };
+
+  const setDarkModeOff = () => {
+    localStorage.theme = "light";
+    updateMode();
+    setMode("light");
+  };
+
   return (
     <div class="flex gap-2 w-full">
-      <Button
-        onClick={() => {
-          localStorage.theme = "dark";
-          updateMode();
-          setMode("dark");
-        }}
-      >
-        <IconMoon />
+      <Button onClick={setDarkModeOn}>
+        <BsMoon />
         Force Dark
       </Button>
 
-      <Button
-        onClick={() => {
-          delete localStorage.theme;
-          updateMode();
-          setMode("auto");
-        }}
-      >
-        <IconSunglasses />
-        Auto
+      <Button onClick={setDarkModeAuto}>
+        <GrSystem />
+        System
       </Button>
 
-      <Button
-        onClick={() => {
-          localStorage.theme = "light";
-          updateMode();
-          setMode("light");
-        }}
-      >
-        <IconSun />
+      <Button onClick={setDarkModeOff}>
+        <BsSun />
         Force light
       </Button>
       <div>
