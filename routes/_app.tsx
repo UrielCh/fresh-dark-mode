0260d3dbf52ac957feb9deb72e4cbe61c276788c
UrielCh <uriel.chemouni@gmail.com>
2022-11-30 11:45:52 +0200

    clean code

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,18 +2,18 @@ import { Head } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/src/server/types.ts";
 
 export default function App({ Component }: AppProps) {
+  const code = `function global_dark(change) {
+    if (change === 'auto') delete localStorage.theme; else if (change === 'on') localStorage.theme = 'dark'; else if (change === 'off') localStorage.theme = 'light';
+    window.isDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    document.documentElement.classList[window.isDark ? 'add' : 'remove']("dark");
+  }
+  global_dark();`;
   return (
     <html>
       <Head>
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-function global_dark(change) {
-  if (change === 'auto') delete localStorage.theme; else if (change === 'on') localStorage.theme = 'dark'; else if (change === 'off') localStorage.theme = 'light';
-  window.isDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-  document.documentElement.classList[window.isDark ? 'add' : 'remove']("dark");
-}
-global_dark();`,
+            __html: code,
           }}
         />
       </Head>
