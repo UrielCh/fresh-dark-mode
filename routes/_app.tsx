ca624f2ab828da0036ce01335be1978c2cc722cf
Uriel Chemouni <uriel.chemouni@gmail.com>
2024-12-27 16:59:39 +0100

    new dark selector, using Typescript.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,19 +1,28 @@
 import { Head } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/src/server/types.ts";
 
+/**
+ * Darkmode function in typescript, will be transpile as js for the browser
+ * @param change pass the new mode as on off or autu
+ */
+const darkModefnc = function global_dark(change?: "on" | "off" | "auto") {
+  if (change === "auto") delete localStorage.theme;
+  else if (change === "on") localStorage.theme = "dark";
+  else if (change === "off") localStorage.theme = "light";
+  const isDark = localStorage.theme === "dark" || (!("theme" in localStorage) && globalThis.matchMedia("(prefers-color-scheme: dark)").matches);
+  document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+  document.dispatchEvent(new Event("dark"));
+  document.cookie = `dark=${isDark ? 1 : 0}; path=/`;
+};
+const codeDark = (darkModefnc.toString() + "global_dark();").replace(";\n", "\n");
+
 export default function App({ Component }: AppProps) {
-  const code = `function global_dark(change) {
-    if (change === 'auto') delete localStorage.theme; else if (change === 'on') localStorage.theme = 'dark'; else if (change === 'off') localStorage.theme = 'light';
-    window.isDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-    document.documentElement.classList[window.isDark ? 'add' : 'remove']("dark");
-  }
-  global_dark();`;
   return (
     <html>
       <Head>
         <script
           dangerouslySetInnerHTML={{
-            __html: code,
+            __html: codeDark,
           }}
         />
       </Head>
